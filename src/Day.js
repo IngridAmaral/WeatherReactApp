7148588b70c7bbb105171758d71a2f3f166a347d
Ingrid Amaral <guid_95@hotmail.com>
2020-01-23 13:35:54 +0100

    weather forecast 6 days

--- src/Day.js
+++ src/Day.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 
 class Day extends Component {
   render() {
+      //date, dateMili, tempMin, tempMax, temperature, weather, weatherDescription
+      
+      //console.log(this.props.date.getDay(), days, weekDay)
+
+      const image = () => {
+          if (this.props.weatherDescription === 'broken clouds') {
+              return 'https://cdn3.iconfinder.com/data/icons/weather-344/142/sunny-512.png'
+          } else if (this.props.weatherDescription === 'overcast clouds') {
+              return 'https://cdn3.iconfinder.com/data/icons/weather-344/142/cloudy-512.png'  
+          } else if (this.props.weatherDescription === 'few clouds') {
+              return 'https://cdn3.iconfinder.com/data/icons/weather-344/142/cloud-512.png'  
+          } else if (this.props.weatherDescription === 'ligth rain') {
+            return 'https://cdn3.iconfinder.com/data/icons/weather-344/142/rain-512.png'
+          } else if (this.props.weatherDescription === 'clear sky') {
+            return 'https://cdn3.iconfinder.com/data/icons/weather-344/142/sun-512.png'
+          } else {
+            return 'https://cdn3.iconfinder.com/data/icons/weather-344/142/sunny-512.png'
+          }
+      }
+
       return (
       <div className='day'>
-        <p>{this.props.week}</p>
-        <img src='https://freesvg.org/img/Anonymous_simple_weather_symbols_1.png' alt={this.props.id} />
+        <p>{this.props.weekDay}</p>
+        <img src={image()} alt={this.props.dateMili} />
         <div>
-            <p>Max: 8</p>
-            <p>Min: 5</p>
-        </div>
+        <p>{this.props.temperature}</p>
+        </div>  
       </div>
     )    
   }
--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,23 +1,66 @@
-import React from 'react';
 import Day from './Day.js'
+import React, { Component } from 'react';
 
-function WeatherDisplay (){
-    const weatherInfos = [
-        {id: 'day01', temp: 25, sky: 'sunny', week: 'Sunday'},
-        {id: 'day02', temp: 25, sky: 'sunny', week: 'Sunday'},
-        {id: 'day03', temp: 25, sky: 'sunny', week: 'Sunday'},
-        {id: 'day04', temp: 25, sky: 'sunny', week: 'Sunday'},
-        {id: 'day05', temp: 25, sky: 'sunny', week: 'Sunday'},
-        {id: 'day06', temp: 25, sky: 'sunny', week: 'Sunday'},
-        {id: 'day07', temp: 25, sky: 'sunny', week: 'Sunday'},
-      ]
+class WeatherDisplay extends Component {
+    state = {
+        info: [],
+    }
 
-    return (
-      <div className="App-display">
-          {weatherInfos.map(curr => {
-              return <Day {...curr} />
-          })}
-      </div>
-    )
+    componentDidMount() {
+        fetch('http://api.openweathermap.org/data/2.5/forecast?q=Berlin,deid=524901&APPID=9da7f2b391643ec2e3f6e8fbdd752534')
+           .then(response => {
+           return response.json()
+         })
+         .then(data => {
+       
+           // Work with JSON data here
+           //console.log(data.list.map(data);
+       
+           const usefulInfo = data.list.map((curr, idx) => {
+               const date = new Date(curr.dt * 1000)
+               const dateMili = curr.dt
+               const temperature = temperatureConverter(curr.main.temp)
+               const tempMin = temperatureConverter(curr.main.temp_min)
+               const tempMax = temperatureConverter(curr.main.temp_max)
+               const weather = curr.weather[0].main
+               const weatherDescription = curr.weather[0].description
+
+               function temperatureConverter(valNum) {
+                   return Math.floor(parseFloat(valNum)-273.15)+'°C';
+               }
+       
+               return {date, dateMili, tempMin, tempMax, temperature, weather, weatherDescription}
+           })
+           //....
+
+           this.setState({ info: usefulInfo })
+         })
+         
+    }
+
+    render() { 
+       const weekWeather = []
+
+       const weather = (this.state.info).forEach((curr, idx, info) => {
+                //console.log()
+                const next = idx < info.length-1 ? info[idx+1].date.getDate() : ''
+                if (curr.date.getDate() !== next) {
+                    weekWeather.push(curr)
+                }
+        })
+
+        console.log(this.state.info)
+
+       //date, dateMili, tempMin, tempMax, temperature, weather, weatherDescription
+
+        return (
+            <div className="App-display">
+                {weekWeather.map((curr, idx) => {
+                    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+                    return <Day {...curr} weekDay={days[curr.date.getDay()]} key={curr.dateMili} />
+                })}
+            </div>
+        )
+    }
 }
 export default WeatherDisplay;
